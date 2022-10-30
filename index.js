c0eb4cc799ae801b51a4ef4d6eb3623ae4f9a20d
nasshitell@gmail.com <nasshitell@gmail.com>
2022-10-30 19:27:55 +0700

    idk why im doing this on sunday

--- index.js
+++ index.js
@@ -4,37 +4,47 @@ const moment = require('moment')
 const dotenv = require('dotenv')
 dotenv.config()
 
-const LIST_OF_HOURS = [
-    { id: 1, hour: "8", message: "Working" },
-    { id: 2, hour: "9", message: "Working" },
-    { id: 3, hour: "10", message: "Workingzz" },
-    { id: 4, hour: "11", message: "Workingz" },
-    { id: 5, hour: "12", message: "Rest" },
-    { id: 6, hour: "13", message: "Working" },
-    { id: 7, hour: "14", message: "Working" },
-    { id: 8, hour: "15", message: "Working" },
-    { id: 9, hour: "16", message: "Working" },
-    { id: 10, hour: "17", message: "After hour" },
-]
+moment.locale('id') // this is for indonesian time zone
+
+const DISCORD_API_BASE_URL = process.env.DISCORD_API_BASE_URL
+const DISCORD_API_GUILD_ID = process.env.DISCORD_API_GUILD_ID
 
 const runDiscordFunction = async () => {
-    for (let i = 0; i < LIST_OF_HOURS.length; i++) {
-        const hour = LIST_OF_HOURS[i]
-        const date = new Date()
-        const currentHour = moment(date).format('HH')
-        if (hour.hour === currentHour) {
-            const response = await setDiscordMessage(hour.message)
-            console.log(response)
-        }
-        console.log("Function Done.")
+
+    const hitEventList = 
+        await axios.get(`${DISCORD_API_BASE_URL}/guilds/${DISCORD_API_GUILD_ID}/scheduled-events?${process.env.DISCORD_API_GUILD_PARAMS}`, {
+            headers: {
+                'Authorization': process.env.AUTHORIZATION_TOKEN,
+                'Content-Type': 'application/json'
+            }
+        })
+    
+    const date = new Date()
+    const currentTime = moment(date)
+
+    if (hitEventList.data.length > 0) {
+        for (var i = 0; i < hitEventList.data.length; i++) {
+            const event = hitEventList.data[i]
+            const eventStartTime = moment(event.scheduled_start_time)
+            const eventEndTime = moment(event.scheduled_end_time)
+            if (currentTime >= eventStartTime && currentTime <= eventEndTime) {
+                await setDiscordMessage(event.name, eventEndTime)
+            }
+        }   
+    } else {
+        await setDiscordMessage("After hour")
     }
 }
 
-const setDiscordMessage = async (message) => {
+const setDiscordMessage = async (message, eventEndTime = moment("2022-02-02 08:00:00")) => {
     try {
+        let finalMessage = `${message}, until ${moment(eventEndTime).format('HH:mm')}`
+        if (message == "After hour") {
+            finalMessage = "After hour 😴"
+        }
         await axios.patch('https://discord.com/api/v9/users/@me/settings', {
             "custom_status": {
-                "text": message,
+                "text": finalMessage,
             },
         }, {
             headers: {
@@ -48,7 +58,7 @@ const setDiscordMessage = async (message) => {
     }
 }
 
-schedule.scheduleJob('* * * * * *', async function(){
+schedule.scheduleJob('*/5 * * * *', async function(){
     console.log('its running.')
     await runDiscordFunction()
 });
